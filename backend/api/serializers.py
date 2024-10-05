a02cca16c8bf9e9fe1efcaf44b79ab4895a9f265
Karestwww <Karestwww@yandex.ru>
2024-10-05 07:36:50 +0300

    сделал заготовки серилайзеров и вьюсетов. Надо сначала добавить urls.

---
+++ backend/api/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import (CurrentUserDefault,
+                                        ModelSerializer,
+                                        SlugRelatedField,
+                                        ValidationError)
+from rest_framework.validators import UniqueTogetherValidator
+
+from recipes.models import Tag, Ingredient, Recipe, Chosen, ShoppingList
+
+User = get_user_model()
+
+
+class TagSerializer(ModelSerializer):
+    pass
+
+
+class IngredientSerializer(ModelSerializer):
+    pass
+
+
+class RecipeSerializer(ModelSerializer):
+    pass
+
+
+class ChosenSerializer(ModelSerializer):
+    pass
+
+
+class ShoppingListSerializer(ModelSerializer):
+    pass
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from api.serializers import (TagSerializer,
+                             IngredientSerializer,
+                             RecipeSerializer,
+                             ChosenSerializer,
+                             ShoppingListSerializer)
+from recipes.models import Tag, Ingredient, Recipe, Chosen, ShoppingList
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class TagsViewSet(ModelViewSet):
+    """По модели POST все стандартные виды запросов через viewsets."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientsViewSet(ModelViewSet):
+    """По модели POST все стандартные виды запросов через viewsets."""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class RecipesViewSet(ModelViewSet):
+    """По модели POST все стандартные виды запросов через viewsets."""
+
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class ChosensViewSet(ModelViewSet):
+    """По модели POST все стандартные виды запросов через viewsets."""
+
+    queryset = Chosen.objects.all()
+    serializer_class = ChosenSerializer
+
+
+class ShoppingsListViewSet(ModelViewSet):
+    """По модели POST все стандартные виды запросов через viewsets."""
+
+    queryset = ShoppingList.objects.all()
+    serializer_class = ShoppingListSerializer
